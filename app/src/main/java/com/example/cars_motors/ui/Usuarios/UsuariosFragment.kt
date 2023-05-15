1d45e828aaf74378570a6fd66b4a5c08bfca7b50
DJeffry.Jeffry <DJeffry.d.jeffru@gmail.com>
2023-05-15 00:10:51 -0600

    Revert "Revert "Cruds faltantes a medias""
    This reverts commit ee432e45902f568ff66e1c7070c65c0cb8b34b51.

--- app/src/main/java/com/example/cars_motors/ui/Usuarios/UsuariosFragment.kt
+++ app/src/main/java/com/example/cars_motors/ui/Usuarios/UsuariosFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.cars_motors.R
 import com.example.cars_motors.controladores.UsuariosController
 import com.example.cars_motors.databinding.FragmentUsuariosBinding
 
@@ -26,18 +29,23 @@ class UsuariosFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(UsuariosViewModel::class.java)
 
         _binding = FragmentUsuariosBinding.inflate(inflater, container, false)
         val root: View = binding.root
         val usuariosController = UsuariosController(requireContext())
         val listaUsuarios = usuariosController.getAllUsuarios().toMutableList()
+        val btnAdd = binding.btnAAdir
+
 
         recyclerView = binding.recyclerViewGroupList
         recyclerView.layoutManager = LinearLayoutManager(activity)
         grupoAdapter = UsuariosAdapter(requireContext(), listaUsuarios, usuariosController)
         recyclerView.adapter = grupoAdapter
+
+        btnAdd.setOnClickListener {
+            val navController = (requireActivity() as AppCompatActivity).findNavController(R.id.nav_host_fragment_content_main)
+            navController.navigate(R.id.formulario_usuarios)
+        }
 
         return root
     }
